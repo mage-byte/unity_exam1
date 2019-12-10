1cf3c5a781094cd5b13f64b20f800f0af01158cf
WoongJang <Woong.Jang@partnet.bmw.co.kr>
2019-12-10 17:58:51 +0900

    commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public ParticleSystem explosionParticle;
+    public AudioSource explosionAudio;
+    public float maxDamage = 100f;
+    public float explosionForce = 1000f;
+    public float lifeTime = 10f;
+    public float explosionRadius = 20f;
+
+    private void Start()
+    {
+        Destroy(gameObject,lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        explosionParticle.transform.parent = null;
+        explosionParticle.Play();
+        explosionAudio.Play();
+
+        Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
+
+        Destroy(gameObject);
+    }
+}
