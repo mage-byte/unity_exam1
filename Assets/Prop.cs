1d35523f41994abddd24e9056b0fc3b89de3173e
WoongJang <Woong.Jang@partnet.bmw.co.kr>
2019-12-17 11:30:17 +0900

    prop

---
+++ Assets/Prop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score = 5;
+    public ParticleSystem explosionParticle;
+    public float hp = 10f;
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+
+        if(hp <= 0)
+        {
+            ParticleSystem instance = Instantiate(explosionParticle,this.transform.position,this.transform.rotation);
+            Destroy(instance.gameObject, instance.main.duration);
+        }
+    }
+}
